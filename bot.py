d04085335ed43350f4baaf3d04c8a89c91e3e38b
= <20002mc@gmail.com>
2020-04-23 18:16:04 +0300

    dialogss

--- bot.py
+++ bot.py
@@ -32,7 +32,7 @@ def any_msg(message):
 
 @bot.message_handler(commands=["read"])
 def read_chat(message):
-    bot.send_message(message.from_user.id, str(list(i for i in client.iter_dialogs()))
+    bot.send_message(message.from_user.id, str(list(i for i in client.iter_dialogs())))
     bot.send_message(message.from_user.id, "Введите название чата")
 
     def get_chat(message):
